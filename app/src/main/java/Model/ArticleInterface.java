5468f57731a4beb9946d3f8635d8bc5df7152f46
CapitainMorgan <40363984+CapitainMorgan@users.noreply.github.com>
2021-12-03 16:41:31 +0100

    Modify Interface

--- app/src/main/java/Model/ArticleInterface.java
+++ app/src/main/java/Model/ArticleInterface.java
@@ -3,10 +3,12 @@ package Model;
 import Data.Article;
 import Data.Category;
 
+import java.util.ArrayList;
+
 public interface ArticleInterface {
     /***
      * @param categories Tableau des catégories à filter
      * @return Un tableau avec tous les articles qui possède au moins une des catégories
      */
-    public Article[] getArticleByCatergories(Category[] categories);
+    public ArrayList<Article> getArticleByCatergories(Category[] categories);
 }
