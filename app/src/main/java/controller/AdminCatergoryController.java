4de99e83d590fb4ab2aa6bc2fd0e38f89a9e1501
CapitainMorgan <matthieugodi@vtx.ch>
2022-01-22 16:22:35 +0100

    Refactor verification of Auth

--- app/src/main/java/controller/AdminCatergoryController.java
+++ app/src/main/java/controller/AdminCatergoryController.java
@@ -19,25 +19,32 @@ import java.util.ArrayList;
 public class AdminCatergoryController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        if(checkAuth(req, resp)) {
+        HttpSession session = req.getSession();
+        try {
+            if(AuthorizationService.isUserAllowed("category.jsp", session.getAttribute("jwt").toString())) {
 
-            String[] stringId = req.getPathInfo().split("/");
+                String[] stringId = req.getPathInfo().split("/");
 
-            Category category = null;
-            ArrayList<Article> articles = new ArrayList<>();
-            int id = 0;
-            try {
-                id = Integer.parseInt(stringId[1]);
-                category = new CategoryQueries().getCategoryById((long) id);
-                Category finalCategory = category;
-                articles = new ArticleQueries().getArticleByCatergories(new ArrayList<Category>() {{add(finalCategory);}});
-            } catch (Exception e) {}
+                Category category = null;
+                ArrayList<Article> articles = new ArrayList<>();
+                int id = 0;
+                try {
+                    id = Integer.parseInt(stringId[1]);
+                    category = new CategoryQueries().getCategoryById((long) id);
+                    Category finalCategory = category;
+                    articles = new ArticleQueries().getArticleByCatergories(new ArrayList<Category>() {{add(finalCategory);}});
+                } catch (Exception e) {}
 
-            req.setAttribute("category", category);
-            req.setAttribute("articles", articles);
-            req.getRequestDispatcher("../WEB-INF/jsps/category.jsp").forward(req, resp);
+                req.setAttribute("category", category);
+                req.setAttribute("articles", articles);
+                req.getRequestDispatcher("../WEB-INF/jsps/category.jsp").forward(req, resp);
 
-        }else{
+            }else{
+                req.setAttribute("msg", "error: access denied");
+                resp.sendRedirect(req.getContextPath() + "/home");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
             req.setAttribute("msg", "error: access denied");
             resp.sendRedirect(req.getContextPath() + "/home");
         }
@@ -45,38 +52,47 @@ public class AdminCatergoryController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        checkAuth(req, resp);
+        HttpSession session = req.getSession();
+        try {
+            if(AuthorizationService.isUserAllowed("category.jsp", session.getAttribute("jwt").toString())) {
 
-        if(req.getParameter("id") != null) {
-            if(!new CategoryQueries().updateCategory((long) Integer.parseInt(req.getParameter("id")), req.getParameter("name")))
-            {
-                ArrayList<Category> categories = new CategoryQueries().getAllCategory();
+                if (req.getParameter("id") != null) {
+                    if (!new CategoryQueries().updateCategory((long) Integer.parseInt(req.getParameter("id")), req.getParameter("name"))) {
+                        ArrayList<Category> categories = new CategoryQueries().getAllCategory();
 
-                req.setAttribute("categories", categories);
-                req.setAttribute("msg", "error: doublon");
-                req.getRequestDispatcher("../WEB-INF/jsps/categories.jsp").forward(req, resp);
-            }else {
-                ArrayList<Category> categories = new CategoryQueries().getAllCategory();
+                        req.setAttribute("categories", categories);
+                        req.setAttribute("msg", "error: doublon");
+                        req.getRequestDispatcher("../WEB-INF/jsps/categories.jsp").forward(req, resp);
+                    } else {
+                        ArrayList<Category> categories = new CategoryQueries().getAllCategory();
 
-                req.setAttribute("categories", categories);
-                req.setAttribute("msg", "success: Category modify");
-                req.getRequestDispatcher("../WEB-INF/jsps/categories.jsp").forward(req, resp);
+                        req.setAttribute("categories", categories);
+                        req.setAttribute("msg", "success: Category modify");
+                        req.getRequestDispatcher("../WEB-INF/jsps/categories.jsp").forward(req, resp);
+                    }
+                } else {
+                    if (!new CategoryQueries().createCategory(req.getParameter("name"))) {
+                        ArrayList<Category> categories = new CategoryQueries().getAllCategory();
+
+                        req.setAttribute("categories", categories);
+                        req.setAttribute("msg", "error: doublon");
+                        req.getRequestDispatcher("../WEB-INF/jsps/categories.jsp").forward(req, resp);
+                    } else {
+                        ArrayList<Category> categories = new CategoryQueries().getAllCategory();
+
+                        req.setAttribute("categories", categories);
+                        req.setAttribute("msg", "success: Category added");
+                        req.getRequestDispatcher("../WEB-INF/jsps/categories.jsp").forward(req, resp);
+                    }
+                }
+            }else{
+                req.setAttribute("msg", "error: access denied");
+                resp.sendRedirect(req.getContextPath() + "/home");
             }
-        }
-        else {
-            if(!new CategoryQueries().createCategory(req.getParameter("name"))) {
-                ArrayList<Category> categories = new CategoryQueries().getAllCategory();
-
-                req.setAttribute("categories", categories);
-                req.setAttribute("msg", "error: doublon");
-                req.getRequestDispatcher("../WEB-INF/jsps/categories.jsp").forward(req, resp);
-            }else {
-                ArrayList<Category> categories = new CategoryQueries().getAllCategory();
-
-                req.setAttribute("categories", categories);
-                req.setAttribute("msg", "success: Category added");
-                req.getRequestDispatcher("../WEB-INF/jsps/categories.jsp").forward(req, resp);
-            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            req.setAttribute("msg", "error: access denied");
+            resp.sendRedirect(req.getContextPath() + "/home");
         }
 
         resp.sendRedirect(req.getContextPath() + "/categories");
@@ -85,7 +101,7 @@ public class AdminCatergoryController extends HttpServlet {
     private boolean checkAuth(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession();
         try {
-            if (AuthorizationService.isUserAllowed("category.jsp", session.getAttribute("jwt").toString())) {
+            if () {
                 return true;
             }
 
